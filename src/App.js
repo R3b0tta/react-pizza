5253919f469869d41be3f1167af22e7f0aa193ac
Rebotta Yolo <125689322+R3b0tta@users.noreply.github.com>
2025-01-14 01:40:29 +0300

    init: reinitializated broken project

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./scss/app.scss";
+import Header from "./components/Header";
+import Categories from "./components/Categories";
+import Sort from "./components/Sort";
+import Pizza from "./components/Pizza";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <Pizza title="Мексиканская" price={490} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
