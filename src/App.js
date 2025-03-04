4f31bac67b7c0312a2b9dda4ef227c03214a324c
Rebotta Yolo <125689322+R3b0tta@users.noreply.github.com>
2025-03-04 19:57:23 +0300

    feat: added router and fix part of adaptive

--- src/App.js
+++ src/App.js
@@ -1,37 +1,22 @@
 import React from "react";
 import "./scss/app.scss";
 import Header from "./components/Header";
-import Categories from "./components/Categories";
-import Sort from "./components/Sort";
-import Pizza from "./components/Pizza";
-import Skeleton from "./components/Pizza/Skeleton";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import NotFound from "./pages/NotFound";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const [items, setItems] = React.useState([]);
-  const [isLoading, setIsLoading] = React.useState(true);
-  React.useEffect(() => {
-    fetch("https://6785cbe9f80b78923aa47299.mockapi.io/api/react-pizza/pizzas")
-      .then((res) => res.json())
-      .then((json) => {
-        setItems(json);
-        setIsLoading(false);
-      });
-  }, []);
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {isLoading
-              ? [...new Array(6)].map((_, i) => <Skeleton key={i} />)
-              : items.map((pizza) => <Pizza key={pizza.id} {...pizza} />)}
-          </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </div>
       </div>
     </div>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Categories from "../components/Categories";
+import Sort from "../components/Sort";
+import Skeleton from "../components/Pizza/Skeleton";
+import Pizza from "../components/Pizza";
+
+const Home = () => {
+  const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+  React.useEffect(() => {
+    fetch("https://6785cbe9f80b78923aa47299.mockapi.io/api/react-pizza/pizzas")
+      .then((res) => res.json())
+      .then((json) => {
+        setItems(json);
+        setIsLoading(false);
+      });
+  }, []);
+  return (
+    <>
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map((_, i) => <Skeleton key={i} />)
+          : items.map((pizza) => <Pizza key={pizza.id} {...pizza} />)}
+      </div>
+    </>
+  );
+};
+export default Home;
