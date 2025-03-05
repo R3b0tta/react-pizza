9a7ef662ffb79ae4a8a4e0699597a78fff009435
Rebotta Yolo <125689322+R3b0tta@users.noreply.github.com>
2025-03-05 15:16:25 +0300

    fix: fixed adaptive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,16 +7,20 @@ import Pizza from "../components/Pizza";
 const Home = () => {
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
+  const url =
+    "https://6785cbe9f80b78923aa47299.mockapi.io/api/react-pizza/pizzas";
+  url.searchParams.append("sortBy", "title");
   React.useEffect(() => {
     fetch("https://6785cbe9f80b78923aa47299.mockapi.io/api/react-pizza/pizzas")
       .then((res) => res.json())
       .then((json) => {
         setItems(json);
         setIsLoading(false);
+        window.scrollTo(0, 0);
       });
   }, []);
   return (
-    <>
+    <div className="container">
       <div className="content__top">
         <Categories />
         <Sort />
@@ -27,7 +31,7 @@ const Home = () => {
           ? [...new Array(6)].map((_, i) => <Skeleton key={i} />)
           : items.map((pizza) => <Pizza key={pizza.id} {...pizza} />)}
       </div>
-    </>
+    </div>
   );
 };
 export default Home;
